3b854e522b8600e3acba939a55a6215c60b3c505
Diego Porres <daporres@ufm.edu>
2019-12-18 18:12:54 +0100

    Now we can simply run ./project_generated_video.sh 3, 3 being run no.

--- projecting_generated_video.sh
+++ projecting_generated_video.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# input: ./projecting_real_video.sh 5
+# input: ./projecting_generated_video.sh 5
 
-# 5 will be the run number (e.g., 00005-project-real-images)
+# 5 will be the run number (e.g., 00005-project-generated-images)
 
 # Run dir:
 printf -v RUN "%05d" "$1"
-RUN="$PWD"/results/"$RUN"-project-real-images
+RUN="$PWD"/results/"$RUN"-project-generated-images
 
 # Duration of video (at 1000 frames, default gives 50fps; change at your own risk):
 DUR=20
@@ -16,7 +16,7 @@ NUMIMAGES=$(ls "$RUN" | grep step | wc -l)
 END=$(("$NUMIMAGES"/1000 - 1))
 
 # Start of our loop
-START=10
+START=0
 
 #e.g., if we have 5000 images, we projected 5 images, so our loop will be from 0 to 4
 # Thus, for each image, from image0000 to image0004:
@@ -35,17 +35,17 @@ do
 	NUMFRAMES=$(($(ls -lR "$RES"/*.png | wc -l) - 1)) # we remove the target image
 	FRAMERATE=$(("$NUMFRAMES" / "$DUR"))
 
-	# Make left video, with bottom box printing frame number (can be easily removed):
+    # Make left video, with bottom box printing frame number (can be easily removed):
     echo -e "\vMaking \vleft \vvideo \vof \v$IMG"
 	ffmpeg -y -framerate "$FRAMERATE" -i "$RES"/"$IMG"-step%04d.png -vf "drawtext=fontfile=DejaVuSansMono.ttf: text='Step\: %{frame_num}': start_number=1: x=(w-tw)/2: y=h-(2*lh): fontcolor=black: fontsize=20: box=1: boxcolor=white: boxborderw=5" -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p "$RES"/left.mp4
 
-	# Make right video:
+    # Make right video:
     echo -e "\vMaking \vright \vvideo \vof \v$IMG"
-	ffmpeg -y -loop 1 -i "$RES"/"$IMG"-target.png -vf "drawtext=fontfile=DejaVuSansMono.ttf: text='Real image': x=(w-tw)/2: y=h-(2*lh): fontcolor=black: fontsize=20: box=1: boxcolor=white: boxborderw=5" -c:v libx264 -t "$DUR" -pix_fmt yuv420p "$RES"/right.mp4
+	ffmpeg -y -loop 1 -i "$RES"/"$IMG"-target.png -vf "drawtext=fontfile=DejaVuSansMono.ttf: text='Generated image': x=(w-tw)/2: y=h-(2*lh): fontcolor=black: fontsize=20: box=1: boxcolor=white: boxborderw=5" -c:v libx264 -t "$DUR" -profile:v high -crf 20 -pix_fmt yuv420p "$RES"/right.mp4
 
-	# Combine left and right videos:
+    # Combine left and right videos:
     echo -e "\vCombining \vvideos \vof \v$IMG"
-	ffmpeg -y -i "$RES"/left.mp4 -i "$RES"/right.mp4 -filter_complex '[0:v][1:v]hstack[vid]' -map [vid] -c:v libx264 -crf 22 -preset veryfast "$RES"/"$IMG"-projecting.mp4
+	ffmpeg -y -i "$RES"/left.mp4 -i "$RES"/right.mp4 -filter_complex '[0:v][1:v]hstack[vid]' -map [vid] -c:v libx264 -crf 20 -preset veryfast "$RES"/"$IMG"-projecting.mp4
 
 	# Delete right and left videos (remove if you wish to keep them):
 	rm -f "$RES"/left.mp4 "$RES"/right.mp4
